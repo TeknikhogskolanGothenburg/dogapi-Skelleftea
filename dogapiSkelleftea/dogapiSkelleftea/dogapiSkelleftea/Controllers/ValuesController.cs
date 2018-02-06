fe0e923aedc6b1dca5d5d7fd907f92e87653b110
Michae Alamin <michaelalamin@gmail.com>
2018-02-06 11:43:15 +0100

    Project Clue

--- dogapiSkelleftea/dogapiSkelleftea/dogapiSkelleftea/Controllers/ValuesController.cs
+++ dogapiSkelleftea/dogapiSkelleftea/dogapiSkelleftea/Controllers/ValuesController.cs
@@ -20,6 +20,7 @@ namespace dogapiSkelleftea.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
+            ////https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.ienumerable-1?f1url=https%3A%2F%2Fmsdn.microsoft.com%2Fquery%2Fdev15.query%3FappId%3DDev15IDEF1%26l%3DEN-US%26k%3Dk(System.Collections.Generic.IEnumerable%601);k(DevLang-csharp)%26rd%3Dtrue&view=netframework-4.7.1
             return dogs.Select(d => d.BreedName).ToArray();
         }
 
@@ -28,6 +29,7 @@ namespace dogapiSkelleftea.Controllers
         public string Get(string id)
         {
             return dogs.Where(x => x.BreedName == id).Select(dog => JsonConvert.SerializeObject(dog)).FirstOrDefault();
+            //https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable.firstordefault?f1url=https%3A%2F%2Fmsdn.microsoft.com%2Fquery%2Fdev15.query%3FappId%3DDev15IDEF1%26l%3DEN-US%26k%3Dk(System.Linq.Enumerable.FirstOrDefault%60%601);k(DevLang-csharp)%26rd%3Dtrue&view=netframework-4.7.1
         }
 
 
